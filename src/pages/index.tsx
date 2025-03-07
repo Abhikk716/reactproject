2aac709c32cffef53890c4dcb46305c345205a7e
Abhikk716 <abhi.kk716@gmail.com>
2025-03-07 11:42:12 +0530

    ddw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,7 +135,7 @@ export const getStaticProps: GetStaticProps = async () => {
     `https://pranaair.com/wp-json/wp/v2/posts?_embed&t=${Date.now()}`
   );
   const productsRes = await fetch(
-    `https://pranaair.com/wp-json/wc/v3/products?consumer_key=ck_d3f818f4910619c28fa8b2ed8f8d3ac81ebcdfd6&consumer_secret=cs_bd45f75058ee51aa277b50115b6bf9ab61416482=${Date.now()}`
+    `https://pranaair.com/wp-json/wc/v3/products?consumer_key=YOUR_CONSUMER_KEY&consumer_secret=YOUR_CONSUMER_SECRET&t=${Date.now()}`
   );
 
   const posts: BlogPost[] = await postsRes.json();
